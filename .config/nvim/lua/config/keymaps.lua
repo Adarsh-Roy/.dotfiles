9201d186659a12579ce528435d322d31f12700ba
Adarsh-Roy <adarshroy.formal@gmail.com>
2025-03-29 14:08:52 +0530

    add my keymaps

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,5 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+vim.keymap.set("n", "<leader>by", "mq<cmd>%y<cr><cmd>delm q<cr>", { desc = "Yank Buffer" })
+vim.keymap.set("n", "<leader>bv", "ggVG", { desc = "Yank Buffer" })
