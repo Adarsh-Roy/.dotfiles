3dcea2d01856238403e2e61a972f6ebab8ad81a6
Adarsh-Roy <79192126+Adarsh-Roy@users.noreply.github.com>
2025-09-03 13:14:26 +0530

    remove unneeded undo dir

--- .config/nvim-self/lua/core/options.lua
+++ .config/nvim-self/lua/core/options.lua
@@ -43,11 +43,4 @@ vim.diagnostic.config({
 })
 
 -- Persistent Undo
-local undo_dir = vim.fn.stdpath('data') .. '/undo'
-
-if vim.fn.isdirectory(undo_dir) == 0 then
-	vim.fn.mkdir(undo_dir, 'p')
-end
-
-vim.opt.undodir = undo_dir
 vim.opt.undofile = true
