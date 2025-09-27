e835c8e0c357a111ea5774bd04f0caf8cebba6d7
Adarsh-Roy <79192126+Adarsh-Roy@users.noreply.github.com>
2025-09-27 09:45:03 +0530

    add nvim-self config

---
+++ .config/nvim-self/lua/plugins/colorscheme.lua
@@ -0,0 +1,16 @@
+return {
+	{
+		"catppuccin/nvim",
+		enabled = true,
+		priority = 1000,
+		config = function()
+			require("catppuccin").setup({
+				flavour = "mocha",
+				transparent_background = true,
+			})
+
+			-- setup must be called before loading
+			vim.cmd.colorscheme "catppuccin"
+		end
+	},
+}
